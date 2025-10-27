902a274a10f227642615b575ddfd298fd0a5cdfa
Onkar Kole <41734467+onkarkole@users.noreply.github.com>
2025-10-27 19:38:44 +0530

    env updated

--- src/main/java/com/qa/utils/SecKeys.java
+++ src/main/java/com/qa/utils/SecKeys.java
@@ -4,22 +4,22 @@ public final class SecKeys {
     private SecKeys() {}
 
     // Encrypted key tokens (no real names visible)
-    public static final String AMFI_ADMIN_USERNAME = "K|f252ab96155fcca405cdc46829e3129966a3a5b84df59e78782f37131f4ef18b";
-    public static final String AMFI_ADMIN_PASSWORD = "K|6a3857a0372f2ee304c14ebe7072f29f3fc7805b4817d03931446dd170041592";
-    public static final String AMFI_TRAINER_USERNAME = "K|c2e969fd87db1b13c49c1780c5368bb0c120b484cc31a1d70f63372845e6c71b";
-    public static final String AMFI_TRAINER_PASSWORD = "K|6728b3c3ceab81635b7d87a543179243e0e75c6ac09cbe88c27681e0825c8242";
-    public static final String EMAIL_CC = "K|e1af9e31d1c0ae5fbe430e7ec702f08dbe60b2f42a6f82e5b3f590fa006352ee";
-    public static final String MAIL_PASSWORD = "K|8442328146e02dc22dfe10a77b5b71b0fcc37e7ca6481e184071ae9c258257c3";
-    public static final String MAIL_PROVIDER = "K|5ac9499da08dfd10d72b0d4b9da7d2c732f8dbca34fb68d4498b7e44267d766f";
-    public static final String MAIL_TO = "K|8ebbde424943ca7b70621d36d37b1c061e3bec3c1fa55874091627d8740b5458";
-    public static final String MAIL_USERNAME = "K|8a6ac7cc34eac3da1697a4740d972b8b773de9c4a516b24cf8556f0401aac30f";
-    public static final String OTP_SENDEREMAIL = "K|d76f14ade89b8711561f416eaaf07914a4b93a7ccf3b27e0764a31cd18d865a0";
-    public static final String OTP_SUBJECT = "K|1b4dd574e74ecfae85deaf32d308111c1a2fd7ee71f94e5bf23bc6d4e66cd145";
-    public static final String TRAINER_USERNAME = "K|eecf14a32ae9ef29873df2c5701788e8005af8b86608e8ff2a2259795deb7fa2";
-    public static final String OTP_LENGTH = "K|74d94fd644b854e3ce9463fccb23f3c468002a7061d880703541018eaa2c29a3";
-    public static final String OTP_PREFERLAST = "K|bc821842451ff8c811aeb1a1ae62311b6e43863eb781a6d0c685078e70bf07ba";
-    public static final String AMFIAPPLICATIONBASEURL = "K|0cd1e049e05b3ca99e4c5285e5124f57de6e4e0869ffe6e315db1904d7eb5107";
-    public static final String ADMINPORTAL = "K|cba05621b16a1e6ebc1545f2e10f1383dc955096cb58a8b96698c1012dfbc01f";
-    public static final String TRAINERPORTAL = "K|d52cc1ae72396479cf3be3e6b4cd29efcf3b90075fbf77a8ec5b762b637a2f32";
-    public static final String BENEFICIARYPORTAL = "K|da30a3f95c242ea62884d61fb1a3e6233d0d08f57ef9e5c92d3a30b667b79e52";
+    public static final String AMFI_ADMIN_USERNAME = "K|50f9344f00e82499b210318e1ef6570270efc0ac185a5729d31385104387c4cc";
+    public static final String AMFI_ADMIN_PASSWORD = "K|3d6e2dd3f886cd81836d94184fae22ba6f2a01087fff84a1857d52b4fd9200d0";
+    public static final String AMFI_TRAINER_USERNAME = "K|92c292a89a97f6da0f8bc6c455ca010e7118658203ec2604c68d8e0c49b21ec2";
+    public static final String AMFI_TRAINER_PASSWORD = "K|4ae80e3cedb7a9a2cb2059279471b7746c86f8fbf0ed56fa44abac2ae47946d7";
+    public static final String EMAIL_CC = "K|3a9288701a3070914fc002c89c2b11049fd7dd105975a3c4ac16a4e1731d3ceb";
+    public static final String MAIL_PASSWORD = "K|c2e2ae94ed4b050e9bdb2fb8d9b1aa96a0595d3720181054a12b06f8cf74ea34";
+    public static final String MAIL_PROVIDER = "K|11f00ce30ecdd0ed83a3e024062231b9860778aa9acbd95130cb8a4713d701b6";
+    public static final String MAIL_TO = "K|3a6d3d538f27e936373744013f14daadb0e31a01a606d778a36b38bde647001d";
+    public static final String MAIL_USERNAME = "K|2e43e95b964dfd9e538212575565ae87cc53cdd8975d09add0de8bdcfcd57ca3";
+    public static final String OTP_SENDEREMAIL = "K|402e7dad34ac3e150865fb66b9fd907871b3f3e807a4164929074d0432b8b817";
+    public static final String OTP_SUBJECT = "K|dce958a6558d08d539f6c32d7a8317da33ed6da65a76206e42a0b9f0e471c48c";
+    public static final String TRAINER_USERNAME = "K|ce6e8d98643933b50a18683868117d76ba80bbd162ba3431b1ce6c3a84596780";
+    public static final String OTP_LENGTH = "K|8ac09f7c227590c6e4ea1337f5db13b07a9515a4499965fb42fe80cdfbe2782a";
+    public static final String OTP_PREFERLAST = "K|49e14a64f49e610419876b62d587eced0d34712468ffcd4970fd7978ec822ea3";
+    public static final String AMFIAPPLICATIONBASEURL = "K|561ee6d61955470d7622970f08fd5941f0ded1077d3ffcf882affb555eebbda7";
+    public static final String ADMINPORTAL = "K|b2aad2bd857fa213d6528b8328df95e413220f5e65ca0fe5d2f5e1812fc8f77d";
+    public static final String TRAINERPORTAL = "K|1160e86026d98c3d5ed038a3b2c7584f733c9417d051deb36ab6159d53d032ff";
+    public static final String BENEFICIARYPORTAL = "K|32124c8ff0cac28cbcc3ad1bf561378db8758fbe4e796bfb1116f479ae82cda0";
 }
